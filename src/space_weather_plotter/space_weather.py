1940c5b2084a8cab8b5fd0756c295c1917f06d26
Vianney <vianneysaadetian@gmail.com>
2024-12-14 17:14:19 +0100

    Fetched more data with the API

--- src/space_weather_plotter/space_weather.py
+++ src/space_weather_plotter/space_weather.py
@@ -6,11 +6,13 @@ and plot the data using Matplotlib.
 """
 import requests  # type: ignore
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import polars as pl
+from polars.exceptions import InvalidOperationError
 
 # Define constants
 BASE_URL = "https://api.nasa.gov/DONKI/"
-API_KEY = "DEMO_KEY"  # Replace with your NASA API key
+API_KEY = "gSCtK7lofcFpG45Luv5rYD8hG2JlMKVbsCp22QbM"  # Replace with your NASA API key
 
 
 # Function to fetch data from NASA API
@@ -83,6 +85,31 @@ def prepare_dataframe(data, columns_mapping, nested_keys=None, timestamp_col=Non
     return df
 
 
+def resample_data(df, time_col, freq):
+    """
+    Resamples the DataFrame to the specified frequency.
+
+    Args:
+        df (pl.DataFrame): The DataFrame to resample.
+        time_col (str): The name of the datetime column.
+        freq (str): The frequency to resample to (e.g., '1d' for daily).
+
+    Returns:
+        pl.DataFrame: The resampled DataFrame.
+    """
+    if df.is_empty():
+        raise ValueError("empty dataframe")
+
+    try:
+        df = df.with_columns(
+            pl.col(time_col).str.strptime(pl.Datetime, format="%Y-%m-%dT%H:%MZ")
+        )
+        df = df.group_by_dynamic(time_col, every=freq).agg(pl.len())
+    except InvalidOperationError as e:
+        raise ValueError("invalid frequency") from e
+    return df
+
+
 # Function to plot data using Polars
 def plot_data(df, x_col, y_col, title, x_label, y_label, kind="line", color="blue"):
     """
@@ -107,14 +134,22 @@ def plot_data(df, x_col, y_col, title, x_label, y_label, kind="line", color="blu
 
     # Plot using Matplotlib
     plt.figure(figsize=(10, 5))
-    if kind == "line":
+    if kind == "line_dot":
         plt.plot(df[x_col], df[y_col], marker="o", linestyle="-", color=color)
+    elif kind == "line":
+        plt.plot(df[x_col], df[y_col], linestyle="-", color=color)
     elif kind == "bar":
-        plt.bar(df[x_col], df[y_col], color=color)
+        plt.bar(df[x_col], df[y_col], width=5, color=color)
 
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+
+    # Limit the number of ticks on the x and y axes
+
+    plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(nbins=20))
+    plt.gca().yaxis.set_major_locator(ticker.MaxNLocator(nbins=20))
+
     plt.xticks(rotation=45)
     plt.grid()
     plt.tight_layout()
@@ -135,17 +170,19 @@ def main():
     Returns:
         None
     """
-    print("Welcome to the Space Weather Observer")
-    print("Fetching current space weather data please wait a few seconds...")
+    print("Welcome to the Space Weather Plotter")
+    print(
+        "Fetching current space weather data from NASA this may take a few minutes..."
+    )
 
     # Fetch and prepare solar flare data
-    flare_params = {"startDate": "2024-01-01", "endDate": "2024-11-23"}
+    flare_params = {"startDate": "2013-01-01", "endDate": "2024-12-12"}
     solar_flares = fetch_data("FLR", flare_params)
     flare_columns = {"beginTime": "time", "classType": "intensity"}
     solar_flares_df = prepare_dataframe(solar_flares, flare_columns, "time")
 
     # Fetch and prepare geomagnetic storm data
-    storm_params = {"startDate": "2024-01-01", "endDate": "2024-11-23"}
+    storm_params = {"startDate": "2010-01-01", "endDate": "2024-12-12"}
     geomagnetic_storms = fetch_data("GST", storm_params)
     storm_columns = {"startTime": "time"}
     storm_nested_col = {"allKpIndex": {"observedTime": "time", "kpIndex": "kp_index"}}
@@ -154,7 +191,7 @@ def main():
     )
 
     # Fetch and prepare solar wind data
-    wind_params = {"startDate": "2024-11-22", "endDate": "2024-11-23"}
+    wind_params = {"startDate": "2013-01-01", "endDate": "2024-12-12"}
     solar_wind = fetch_data("WSAEnlilSimulations", wind_params)
     wind_columns = {"time21_5": "time"}
     wind_nested_col = {"cmeInputs": {"cmeStartTime": "time", "speed": "speed"}}
@@ -169,25 +206,27 @@ def main():
 
         choice = input("Enter your choice (1-4): ")
         if choice == "1":
+            # Resample the data to weekly frequency by counting occurrences
+            solar_flares_resampled_df = resample_data(solar_flares_df, "time", "1w")
             plot_data(
-                solar_flares_df,
+                solar_flares_resampled_df,
                 "time",
-                "intensity",
-                "Solar Flares",
+                "len",
+                "Solar Flares Count over the last Sun cycle",
                 "Time",
-                "Intensity",
+                "Count",
                 kind="bar",
-                color="orange",
+                color="red",
             )
         elif choice == "2":
             plot_data(
                 geomagnetic_storms_df,
                 "time",
                 "kp_index",
-                "Geomagnetic Storms",
+                "Geomagnetic Storms since 2010",
                 "Start Time",
                 "KP Index",
-                kind="line",
+                kind="line_dot",
                 color="blue",
             )
         elif choice == "3":
@@ -195,7 +234,7 @@ def main():
                 solar_wind_df,
                 "time",
                 "speed",
-                "Solar Wind Speeds",
+                "Solar Wind Speeds evolution over the last Sun cycle",
                 "Time",
                 "Speed (km/s)",
                 kind="line",
